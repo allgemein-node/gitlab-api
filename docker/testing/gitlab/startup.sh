cad4f4f1908935a295cca9a19cd1af150cfe02d5
Robert Kania <cezaryrk@yahoo.de>
2020-10-28 17:46:27 +0100

    ci: try travis fix

--- docker/testing/gitlab/startup.sh
+++ docker/testing/gitlab/startup.sh
@@ -20,7 +20,9 @@ if [ ! -f /etc/.gitlab-booted ]; then
   gitlab-ctl stop puma
   gitlab-ctl stop sidekiq
   gitlab-backup restore BACKUP=test force=yes
-  gitlab-ctl restart
+  gitlab-ctl start sidekiq
+  gitlab-ctl start puma
+  gitlab-ctl start unicorn
   sleep 30
   touch /etc/.gitlab-booted
   echo "Install backup => done"
